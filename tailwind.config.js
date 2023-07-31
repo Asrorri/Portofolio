e7ed52168e22c765058f44deefe70fbd7c461f8c
Asrroriiii <sitimarwiyahfbs@gmail.com>
2023-07-31 19:56:17 +0800

    portofolio css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center:true,
+      padding:'16px'
+    },
+    extend: {
+      colors:{
+        primary: '#0ea5e9',
+        secondary:'#64748b',
+        dark: '#0f172a'
+      }
+    },
+  },
+  plugins: [],
+}
+
